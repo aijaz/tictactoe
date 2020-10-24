269a84db357adfd4bcc60c56ebbbaef789133062
Aijaz A. Ansari <aijaz@aijaz.net>
2020-10-23 18:45:51 -0600

    Calculating row and col cleverly

--- main.py
+++ main.py
@@ -12,37 +12,10 @@ print("|".join(["7", "8", "9"]))
 
 while moves_made < 9:
     move = input(f"{current_player}'s turn to make a move. Enter a number from 1 to 9: ")
-    move = int(move)
+    move = int(move) - 1
 
-    row = 0
-    col = 0
-    if move == 1:
-        row = 0
-        col = 0
-    elif move == 2:
-        row = 0
-        col = 1
-    elif move == 3:
-        row = 0
-        col = 2
-    elif move == 4:
-        row = 1
-        col = 0
-    elif move == 5:
-        row = 1
-        col = 1
-    elif move == 6:
-        row = 1
-        col = 2
-    elif move == 7:
-        row = 2
-        col = 0
-    elif move == 8:
-        row = 2
-        col = 1
-    elif move == 8:
-        row = 2
-        col = 2
+    row = int(move / 3)
+    col = move % 3
 
     board[row][col] = current_player
 
@@ -60,10 +33,4 @@ while moves_made < 9:
 
     moves_made += 1
 
-print("|".join(board[0]))
-print("-+-+-")
-print("|".join(board[1]))
-print("-+-+-")
-print("|".join(board[2]))
-print("\n\n")
 
